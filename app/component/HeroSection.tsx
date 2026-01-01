3a1cfce76bab4c7149c30eb406e5471c5e7dbecf
Lahhiya <mdsmrevolution6@gmail.com>
2026-01-01 20:50:38 +0700

    d1

---
+++ app/component/HeroSection.tsx
@@ -0,0 +1,69 @@
+"use client"
+
+export default function HeroSection(){
+    return (
+      <section className="container-lg w-full h-100 flex justify-center ">
+        <div className=" text-center  flex flex-col items-center justify-center">
+          <h1 className="text-center  md-2 text-zinc-900">
+            <span className="text-8xl font-google-sans font-semibold">
+              Welcome
+            </span>
+          </h1>
+          <h2 className="text-center mt-2 mb-5 text-zinc-900">
+            <span className="text-3xl font-lato font-semibold  text-shadow-zinc-500">
+              Lorem ipsum dolor sit amet.
+            </span>
+          </h2>
+          <div className="flex gap-3 justify-center my-2">
+            <button className="flex gap-2 py-2 align-center items-center bg-black text-white border border-gray-800 p-3 px-5 rounded-2xl h-auto font-medium capitalize text-md text-lg hover:bg-zinc-900 hover:text-slate-100 hover:scale-105 cursor-pointer">
+              <span className="align-center">Learn more</span>
+              <span>
+                <svg
+                  className="w-[20px] h-[20px] text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M18 14v4.833A1.166 1.166 0 0 1 16.833 20H5.167A1.167 1.167 0 0 1 4 18.833V7.167A1.166 1.166 0 0 1 5.167 6h4.618m4.447-2H20v5.768m-7.889 2.121 7.778-7.778"
+                  />
+                </svg>
+              </span>
+            </button>
+            <button className="flex gap-2 py-2 align-center items-center bg-slate-100 text-gray-800 border border-gray-800 p-3 px-5 rounded-2xl h-auto font-medium capitalize text-md text-lg hover:bg-slate-50 hover:text-zinc-800 hover:scale-105 cursor-pointer">
+              <span className="align-center">Contact</span>
+              <span>
+                <svg
+                  className="w-[20px] h-[20px] text-gray-800 "
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4 3a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h1v2a1 1 0 0 0 1.707.707L9.414 13H15a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H4Z"
+                    clipRule="evenodd"
+                  />
+                  <path
+                    fillRule="evenodd"
+                    d="M8.023 17.215c.033-.03.066-.062.098-.094L10.243 15H15a3 3 0 0 0 3-3V8h2a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-1v2a1 1 0 0 1-1.707.707L14.586 18H9a1 1 0 0 1-.977-.785Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </span>
+            </button>
+          </div>
+        </div>
+      </section>
+    );
+}
---
+++ app/component/Navbar.tsx
@@ -0,0 +1,22 @@
+"use client"
+import Link from "next/link"
+import Image from "next/image"
+import amiya from "@/public/amiya.svg"
+
+export default function Navbar(){
+    return (
+      <div className="container-lg flex items-center sticky z-20  top-0 max-w-screen mx-auto h-14 md:px-15 px-5 gap-8 justify-between bg-slate-100 shadow-sm shadow-slate-200">
+        <Link className="flex items-center w-2/12 gap-4" href={"/"}>
+          <Image src={amiya} alt="amiya icon" className="w-auto h-8 md:h-10" ></Image>
+          <h1 className="text-md md:text-2xl font-bold uppercase text-stone-800">entah</h1>
+        </Link>
+        <nav className="flex w-8/12 justify-end items-center md:px-10 me-5 gap-5 text-md md:text-lg text-stone-800">
+            <div className="flex w-1/2 justify-between md:gap-15 gap-3 font-semibold">
+                <Link href={"/"} className=" hover:underline hover:text-zinc-600 hover:scale-105 underline">Home</Link>
+                <Link href={"/"} className=" hover:underline hover:text-zinc-600 hover:scale-105">About</Link>
+                <Link href={"/"} className=" hover:underline hover:text-zinc-600 hover:scale-105">Contact</Link>
+            </div>
+        </nav>
+      </div>
+    );
+}
